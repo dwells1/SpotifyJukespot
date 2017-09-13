c9c49189e2e56f7d2ff35204607c871b55adc41a
dwells1 <nique_25.home@yahoo.com>
2017-09-13 13:12:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jukespot/spotifyjukespot/Login.java
@@ -0,0 +1,94 @@
+package com.example.jukespot.spotifyjukespot;
+
+/**
+ * Created by nique on 9/10/2017.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+public class Login extends Activity{
+    private static final String TAG = Login.class.getSimpleName();
+
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String CLIENT_ID = "4309049aaf574f63b61d3408408a4ff2";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String REDIRECT_URI = "jukebox://callback";
+
+    private static final int REQUEST_CODE = 1337;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String token = CredentialsHandler.getToken(this);
+        if (token == null) {
+            setContentView(R.layout.activity_login);
+        } else {
+            startMainActivity(token);
+        }
+    }
+
+    public void onLoginButtonClicked(View view) {
+        System.out.println("login clicked");
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    logMessage("Got token: " + response.getAccessToken());
+                    CredentialsHandler.setToken(this, response.getAccessToken(), response.getExpiresIn(), TimeUnit.SECONDS);
+                    startMainActivity(response.getAccessToken());
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    logError("Auth error: " + response.getError());
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    logError("Auth result: " + response.getType());
+            }
+        }
+    }
+
+    private void startMainActivity(String token) {
+        Intent intent = MainActivity.createIntent(this);
+        //intent.putExtra(MainActivity.EXTRA_TOKEN, token);
+        startActivity(intent);
+        finish();
+    }
+
+    private void logError(String msg) {
+        Toast.makeText(this, "Error: " + msg, Toast.LENGTH_SHORT).show();
+        Log.e(TAG, msg);
+    }
+
+    private void logMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, msg);
+    }
+}
---
+++ app/src/main/java/com/example/jukespot/spotifyjukespot/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.jukespot.spotifyjukespot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.SearchView;
+
+import java.util.List;
+
+import kaaes.spotify.webapi.android.models.Track;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static Intent createIntent(Context context) {
+        return new Intent(context, MainActivity.class);
+    }
+}
+
