a5352aa6d91702187daab9338c7d7ab046cdb248
Lino Perez <linosperez@gmail.com>
2017-09-15 05:54:24 -0500

    Added the basic app skeleton of how the navigation will go through the app but they have no functionality yet... Also, I tried doing a side bar menu in the MainActivity to terrible results so I commented it out  and left it as it was as I work it out, need to research Fragments so that the menu is consistent

--- app/src/main/java/com/example/jukespot/spotifyjukespot/JukeboxUserOptions.java
+++ app/src/main/java/com/example/jukespot/spotifyjukespot/JukeboxUserOptions.java
@@ -1,18 +1,11 @@
 package com.example.jukespot.spotifyjukespot;
 
-/**
- * This will be the regular Login for the App itself and
- * not the Spotify Authentication even though as of now it is
- * still in there....
- * Created by nique on 9/10/2017.
- */
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.concurrent.TimeUnit;
@@ -20,11 +13,10 @@ import java.util.concurrent.TimeUnit;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
+public class JukeboxUserOptions extends Activity {
+    Button btnCreateJukebox;
+    Button btnJoinJukebox;
 
-public class Login extends Activity{
-    Button bRegLogin;
-    EditText edUsername;
-    EditText edPassword;
     private static final String TAG = Login.class.getSimpleName();
 
     @SuppressWarnings("SpellCheckingInspection")
@@ -37,34 +29,36 @@ public class Login extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*TODO: initialize EditText field for username and password and check if inputs are correct*/
-        bRegLogin = (Button) findViewById(R.id.bRegLogin);
-        setContentView(R.layout.activity_login);
-        /*   String token = CredentialsHandler.getToken(this);
-        if (token == null) {
-            setContentView(R.layout.activity_login);
-        } else {
-            startMainActivity(token);
-        } */
+
+        initJukeboxButtons();
+        setContentView(R.layout.activity_jukebox_user_options);
     }
 
-    public void onLoginButtonClicked(View view) {
-        System.out.println("login clicked");
-        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
-                AuthenticationResponse.Type.TOKEN,REDIRECT_URI);
-        builder.setScopes(new String[]{"user-read-private", "streaming"});
-        AuthenticationRequest request = builder.build();
+    public void initJukeboxButtons(){
+        btnCreateJukebox = (Button) findViewById(R.id.bStartJukebox);
+        btnJoinJukebox = (Button) findViewById(R.id.bJoinJukebox);
+    }
+    public void onStartNewJukeboxClicked(View view){
+        String loginToken = CredentialsHandler.getToken(this);
+        if(loginToken == null) {
+            AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                    AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+            builder.setScopes(new String[]{"user-read-private", "streaming"});
+            AuthenticationRequest request = builder.build();
 
-        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+            AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+        }else{
+            //startMainActivity(loginToken);
+            startCreatorJukeboxOptions(loginToken);
+        }
+        Log.v(TAG,"START NEW PRESSED");
     }
-    public void onRegLoginClicked(View view){
-        Log.v(TAG,"LOGIN REG PRESSED");
-        /*TODO: check username and password input here*/
-        /*Assume Password and Username are Correct and go to next instance*/
-        logMessage("Login Successful");
-        startJukeboxOptions();
-    }
-    /*TODO: Remove all mentions of original Spotify Login*/
+
+    public void onJoinJukeboxClicked(View view){
+        /*TODO: add functionality for joining a jukebox button at the moment it does nothing*/
+        Log.v(TAG,"JOIN JUKE PRESSED");
+    }// end onJoinJukeboxClicked
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
@@ -77,7 +71,8 @@ public class Login extends Activity{
                 case TOKEN:
                     logMessage("Got token: " + response.getAccessToken());
                     CredentialsHandler.setToken(this, response.getAccessToken(), response.getExpiresIn(), TimeUnit.SECONDS);
-                    startMainActivity(response.getAccessToken());
+                   //startMainActivity(response.getAccessToken());
+                    startCreatorJukeboxOptions(response.getAccessToken());
                     break;
 
                 // Auth flow returned an error
@@ -90,19 +85,20 @@ public class Login extends Activity{
                     logError("Auth result: " + response.getType());
             }
         }
-    }
+    } //end onActivity
 
     private void startMainActivity(String token) {
         Intent intent = MainActivity.createIntent(this);
         //intent.putExtra(MainActivity.EXTRA_TOKEN, token);
         startActivity(intent);
         finish();
-    }
-    private void startJukeboxOptions(){
-        Intent jukeboxOptionsIntent = new Intent(this, JukeboxUserOptions.class);
-        startActivity(jukeboxOptionsIntent);
-        finish();
+    }//end startMain
 
+    private void startCreatorJukeboxOptions(String token){
+        Intent jukeboxCreatorOptionsIntent = new Intent(this,
+                JukeboxCreationOptions.class);
+        startActivity(jukeboxCreatorOptionsIntent);
+        finish();
     }
     private void logError(String msg) {
         Toast.makeText(this, "Error: " + msg, Toast.LENGTH_SHORT).show();
@@ -113,4 +109,5 @@ public class Login extends Activity{
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         Log.d(TAG, msg);
     }
-}
+
+}// end class
--- app/src/main/java/com/example/jukespot/spotifyjukespot/MainActivity.java
+++ app/src/main/java/com/example/jukespot/spotifyjukespot/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.jukespot.spotifyjukespot;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
@@ -20,11 +33,30 @@ import kaaes.spotify.webapi.android.models.Track;
 
 public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient mFusedLocationClient;
+    /*Drawer Navigation*/
+    private ActionBarDrawerToggle menuDrawerToggle;
+    private String currentSelectionFromMenuTitle;
+    private String[] mainUserOptionsForDrawer;
+    private String currentActivityTitle;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> menuAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Functions for Navigable Menu*/
+       /* mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        currentActivityTitle = getTitle().toString();
+        addItemsToDrawerMenu();
+        setupDrawerMenu();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);*/
+
+
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -49,6 +81,73 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+    /*
+    public void addItemsToDrawerMenu() {
+        mainUserOptionsForDrawer = new String[]{"Search", "Current Queue",
+                "Currently Playing", "End Current Jukebox", "Logout"};
+        menuAdaptor = new ArrayAdapter<String>(this,
+                R.layout.drawer_options_list_layout, mainUserOptionsForDrawer);
+
+        mDrawerList.setAdapter(menuAdaptor);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                currentSelectionFromMenuTitle = ((TextView) view.findViewById(R.id.itemChosen)).getText().toString();
+                Toast.makeText(MainActivity.this, currentSelectionFromMenuTitle, Toast.LENGTH_SHORT).show();
+                selectMenuItem(position);
+            }
+        });
+    }
+    public void selectMenuItem(int position){
+        if(currentSelectionFromMenuTitle.equals("Search")){
+            Fragment currentFrag = new SearchFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragTransaction = fragmentManager.beginTransaction();
+            fragTransaction.add(R.id.content_frame,currentFrag);
+            fragTransaction.commit();
+        }
+
+
+        currentActivityTitle = currentSelectionFromMenuTitle;
+        mDrawerLayout.closeDrawer(mDrawerList);
+
+
+    }
+    public void setupDrawerMenu(){
+        menuDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close){ */
+            /** Called when a drawer has settled in a completely open state. */
+         /*   public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }*/
+
+            /** Called when a drawer has settled in a completely closed state. */
+          /*  public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(currentActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        menuDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(menuDrawerToggle);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        menuDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemID = item.getItemId();
+
+        if (menuDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 
     protected void createLocationRequest() {
         LocationRequest mLocationRequest = new LocationRequest();
