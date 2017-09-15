851542dc986fd3406856daea351ae851cff26344
JLEsparza17 <jorge.esparza15@hotmail.com>
2017-09-15 14:27:23 -0500

    Set up google play service library and first task of getting known location so far more features to add. Currently on receiving location updates.

--- app/src/main/java/com/example/jukespot/spotifyjukespot/MainActivity.java
+++ app/src/main/java/com/example/jukespot/spotifyjukespot/MainActivity.java
@@ -12,7 +12,11 @@ import android.content.Intent;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.List;
 
@@ -44,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
-                            // ...
+                            double latitude = location.getLatitude();
+                            double longitutde = location.getLongitude();
                         }
                     }
                 });
@@ -55,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
         mLocationRequest.setInterval(10000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
     }
 
     public static Intent createIntent(Context context) {
