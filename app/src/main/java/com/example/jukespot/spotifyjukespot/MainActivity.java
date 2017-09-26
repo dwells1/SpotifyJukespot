24aef20c939838396ebb202cc5824b64dec65b98
JLEsparza17 <jorge.esparza15@hotmail.com>
2017-09-25 23:07:06 -0500

    Location Activity and layout setup but still need to test and see if coordinates will display. Also need to connect location and main activity so locations runs.

--- app/src/main/java/com/example/jukespot/spotifyjukespot/MainActivity.java
+++ app/src/main/java/com/example/jukespot/spotifyjukespot/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.jukespot.spotifyjukespot;
 
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-/**TODO:WHEN MAKING A NEW FRAGMENT MAKE SURE THIS VERSION OF FRAGMENT IS IMPORTED IN THAT FILE**/
+/* TODO:WHEN MAKING A NEW FRAGMENT MAKE SURE THIS VERSION OF FRAGMENT IS IMPORTED IN THAT FILE* */
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -28,27 +23,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
-import java.util.List;
-
-import kaaes.spotify.webapi.android.models.Track;
-
 /*TODO: When Adding new Fragments you have to implement them as the ones here*/
-public class MainActivity extends AppCompatActivity implements SearchFragment.OnFragmentInteractionListener, CurrentQueueFragment.OnFragmentInteractionListener{
-    private FusedLocationProviderClient mFusedLocationClient;
+public class MainActivity extends AppCompatActivity implements SearchFragment.OnFragmentInteractionListener, CurrentQueueFragment.OnFragmentInteractionListener {
     /*Drawer Navigation*/
     private ActionBarDrawerToggle menuDrawerToggle;
     private String currentSelectionFromMenuTitle;
@@ -65,39 +41,13 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
 
         /*Functions for Navigable Menu*/
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         currentActivityTitle = getTitle().toString();
         addItemsToDrawerMenu();
         setupDrawerMenu();
         setFirstFragment();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-
-        /*Location Stuff*/
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            double latitude = location.getLatitude();
-                            double longitutde = location.getLongitude();
-                        }
-                    }
-                });
     }
 
     public void addItemsToDrawerMenu() {
@@ -112,36 +62,37 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 currentSelectionFromMenuTitle = ((TextView) view.findViewById(R.id.itemChosen)).getText().toString();
-               // Toast.makeText(MainActivity.this, currentSelectionFromMenuTitle, Toast.LENGTH_SHORT).show();
+                // Toast.makeText(MainActivity.this, currentSelectionFromMenuTitle, Toast.LENGTH_SHORT).show();
                 selectMenuItem(position);
             }
         });
     }
+
     /*TODO: add detection so user cannot press same item twice and just reload*/
-    public void selectMenuItem(int position){
+    public void selectMenuItem(int position) {
         Fragment currentFrag = null;
         boolean isFragmentNeeded = true;
 
         /*TODO: Create Fragments for other menu options except maybe logout*/
-        if(currentSelectionFromMenuTitle.equals("Search")){
+        if (currentSelectionFromMenuTitle.equals("Search")) {
             currentFrag = new SearchFragment();
 
-        }else if(currentSelectionFromMenuTitle.equals("Current Queue")){
+        } else if (currentSelectionFromMenuTitle.equals("Current Queue")) {
             currentFrag = new CurrentQueueFragment();
-        }else if(currentSelectionFromMenuTitle.equals("End Current Jukebox")){
+        } else if (currentSelectionFromMenuTitle.equals("End Current Jukebox")) {
             /*TODO: Add Alert so user confirms ending jukebox*/
-            Toast.makeText(this,"Jukebox Ended",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Jukebox Ended", Toast.LENGTH_SHORT).show();
             Intent jukeboxOptionsIntent = new Intent(this, JukeboxUserOptions.class);
             startActivity(jukeboxOptionsIntent);
             finish();
-        }else if(currentSelectionFromMenuTitle.equals("Logout")){
+        } else if (currentSelectionFromMenuTitle.equals("Logout")) {
             /*TODO: Should this log them out of spotify??*/
-            Toast.makeText(this,"Logout Successfull",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Logout Successfull", Toast.LENGTH_SHORT).show();
             Intent jukeboxLoginIntent = new Intent(this, Login.class);
             startActivity(jukeboxLoginIntent);
             finish();
         }
-        if(currentFrag != null) {
+        if (currentFrag != null) {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragTransaction = null;
             try {
@@ -158,9 +109,9 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
 
     }
 
-    public void setupDrawerMenu(){
+    public void setupDrawerMenu() {
         menuDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                R.string.navigation_drawer_open,R.string.navigation_drawer_close){
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
@@ -179,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
         mDrawerLayout.addDrawerListener(menuDrawerToggle);
     }
 
-    public void setFirstFragment(){
+    public void setFirstFragment() {
         Fragment currentFrag = new SearchFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragTransaction = null;
@@ -187,7 +138,7 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
             fragTransaction = fragmentManager.beginTransaction();
             fragTransaction.replace(R.id.content_frame, currentFrag);
             fragTransaction.commit();
-        }catch(Exception FragNotFound){
+        } catch (Exception FragNotFound) {
             FragNotFound.printStackTrace();
         }
 
@@ -195,19 +146,21 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
         setTitle(currentActivityTitle);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         menuDrawerToggle.syncState();
     }
+
     /*TODO: Check what this does is it needed for loading more complex Fragments??*/
     @Override
-    public void onFragmentInteraction(Uri uri){
+    public void onFragmentInteraction(Uri uri) {
         //you can leave it empty
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         int itemID = item.getItemId();
 
         if (menuDrawerToggle.onOptionsItemSelected(item)) {
@@ -215,13 +168,14 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
         }
         return super.onOptionsItemSelected(item);
     }
+
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event){
-        if((keyCode == KeyEvent.KEYCODE_BACK)){
-            if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
                 mDrawerLayout.closeDrawer(mDrawerList);
                 return true;
-            }else {
+            } else {
                 mDrawerLayout.openDrawer(mDrawerList);
                 return true;
             }
@@ -229,55 +183,6 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
         return super.onKeyDown(keyCode, event);
     }
 
-    protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(mLocationRequest);
-
-        SettingsClient client = LocationServices.getSettingsClient(this);
-        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-
-        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
-            @Override
-            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                // All location settings are satisfied. The client can initialize
-                // location requests here.
-                // ...
-            }
-        });
-
-        task.addOnFailureListener(this, new OnFailureListener() {
-            public static final int REQUEST_CHECK_SETTINGS = 0x1 ;
-
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                int statusCode = ((ApiException) e).getStatusCode();
-                switch (statusCode) {
-                    case CommonStatusCodes.RESOLUTION_REQUIRED:
-                        // Location settings are not satisfied, but this can be fixed
-                        // by showing the user a dialog.
-                        try {
-                            // Show the dialog by calling startResolutionForResult(),
-                            // and check the result in onActivityResult().
-                            ResolvableApiException resolvable = (ResolvableApiException) e;
-                            resolvable.startResolutionForResult(MainActivity.this,
-                                    REQUEST_CHECK_SETTINGS);
-                        } catch (IntentSender.SendIntentException sendEx) {
-                            // Ignore the error.
-                        }
-                        break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        // Location settings are not satisfied. However, we have no way
-                        // to fix the settings so we won't show the dialog.
-                        break;
-                }
-            }
-        });
-    }
 
     public static Intent createIntent(Context context) {
         return new Intent(context, MainActivity.class);
